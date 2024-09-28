185c33b08c8b1997e5d0ebdb88b6495899d747d5
jewynny <jgmh003@gmail.com>
2024-09-28 16:24:44 -0700

    Added a simple menu function to display choices

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+void menu();
+
 int main() {
-    cout << "Hello world" << endl;
+    
+}
+
+void menu() {
+    cout << "Welcome to [COMPANY] Login." << endl;
+    cout << "---------------------------" << endl;
+    cout << "Select an option:" << endl;
+    cout << "1. Create Account" << endl;
+    cout << "2. Log in" << endl;
+    cout << "3. Exit" << endl;
+    cout << "Enter choice by typing 1-3: ";
 }
