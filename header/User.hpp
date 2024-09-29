d69488287248b1e197181ea3f776ddc2a269deac
jewynny <jgmh003@gmail.com>
2024-09-28 17:17:02 -0700

    Fleshing out user class

--- header/User.hpp
+++ header/User.hpp
@@ -4,14 +4,28 @@
 
 using namespace std;
 
+enum Month{JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER};
+
 class User {
     public: 
-    enum Month{JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER};
-    void getUserChoice(char userChoice);
+    // enum Month{JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER};
+    // void getUserChoice(char userChoice);
+
+    string getUserUsername();
+    string getUserPassword();
+    int getuserBirthDate();
+    int getUserBirthYear();
+    Month getUserBirthMonth();
+
+    void setUserUsername(string username);
+    void setUserPassword(string password);
+    void setuserBirthDate(int birthDate);
+    void setUserBirthYear(int birthYear);
+    void setUserBirthMonth(Month birthMonth);
     
     private:
     char userChoice;
-    string userName; // will be used for lookup when logging in
+    string username; // will be used for lookup when logging in
 
     // for if there are age rstrictions
     string password; // not encrypted
--- src/User.cpp
+++ src/User.cpp
@@ -1,6 +1,34 @@
 #include "../header/User.hpp"
 
-void User::getUserChoice(char userChoice)
-{
-    
-}
+//GETTER FUNCTOINS
+string User::getUserUsername() {
+    return username;}
+
+string User::getUserPassword() {
+    return password;}
+
+int User::getuserBirthDate() {
+    return birthDate;}
+
+int User::getUserBirthYear() {
+    return birthYear;}
+
+Month User::getUserBirthMonth() {
+    return birthMonth;}
+
+
+//SETTER FUNCTIONS
+void User::setUserUsername(string username) {
+    this->username = username;} // class username = inputted username
+
+void User::setUserPassword(string password) {
+    this->password = password;}
+
+void User::setuserBirthDate(int birthDate) {
+    this->birthDate = birthDate;}
+
+void User::setUserBirthYear(int birthYear) {
+    this->birthYear = birthYear;}
+
+void User::setUserBirthMonth(Month birthMonth) {
+    this->birthMonth = birthMonth;}
